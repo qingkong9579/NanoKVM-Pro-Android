267262bdf776ac6c830467d37ce894fb7758210b
微茫之心 <2597924209@qq.com>
2026-09-05 23:41:10 +0800

    release: v0.1.2 (theme fixes)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.nanokvm.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
     }
 
     signingConfigs {
