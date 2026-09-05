86a37f1cf39674362d43a9b2fb657a76911ea637
微茫之心 <2597924209@qq.com>
2026-09-05 17:02:23 +0800

    build: ABI split APKs (4 arch + universal)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,15 @@ android {
         }
     }
 
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = true
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = true
