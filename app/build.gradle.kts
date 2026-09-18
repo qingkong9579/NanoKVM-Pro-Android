f15624813c8bec591b6f299316ea06c47b9e40ff
微茫之心 <2597924209@qq.com>
2026-09-18 22:36:24 +0800

    release: v0.1.4-hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.nanokvm.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.1.4-hotfix"
     }
 
     signingConfigs {
