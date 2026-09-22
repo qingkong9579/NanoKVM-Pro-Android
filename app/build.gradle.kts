b7c6bc5a07721affee1479ac4b5b1946b4c728b4
微茫之心 <2597924209@qq.com>
2026-09-22 16:10:32 +0800

    release: v0.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.nanokvm.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.1.6"
+        versionCode = 9
+        versionName = "0.1.7"
     }
 
     signingConfigs {
